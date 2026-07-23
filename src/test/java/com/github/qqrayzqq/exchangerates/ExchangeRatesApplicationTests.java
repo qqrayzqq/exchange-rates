c6e48028892b743bb39ad1b70f5cb420b58fd1ba
qqrayzqq <162329881+qqrayzqq@users.noreply.github.com>
2026-07-23 20:02:43 +0200

    chore: bootstrap Spring Boot project via Spring Initializr

---
+++ src/test/java/com/github/qqrayzqq/exchangerates/ExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qqrayzqq.exchangerates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
